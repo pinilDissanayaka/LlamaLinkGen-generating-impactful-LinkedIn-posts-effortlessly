b22ce3602a04c6433350bc3da9f0a4162a1a68ca
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-28 11:33:37 +0530

    add the market researcher

--- nodes/market_research.py
+++ nodes/market_research.py
@@ -1,29 +1,50 @@
 from langchain_core.prompts import PromptTemplate
+from utils import llm, GraphState
 from datetime import datetime
-
-market_researcher_prompt_template="""
-    You are an expert at Investigate the latest trends.
-    Investigate the latest trends, hashtags, and competitor activities on 
-    Linkedin specific to the industry of this Linkedin account. 
-    Focus on gathering data that reveals what content performs well in the current year, 
-    identifying patterns, preferences, and emerging trends. 
-
-        Current date: {CURRENT_DATE}
-
-    Description of the Linkedin account for which you are doing this research: 
-        Linkedin Account Description : {LINKEDIN_ACCOUNT_DESCRIPTION}
-
-    Find the most relevant topics, hashtags and trends to use the the posts for next week. 
-"""
-
-
-market_researcher_prompt = PromptTemplate.from_template(
-    template=market_researcher_prompt_template
-)
+from langchain_core.runnables import RunnablePassthrough
+from  langchain_core.output_parsers import JsonOutputParser
 
 
 
-print(datetime.now().strftime("%Y-%m-%d"))
+def market_researcher(state: GraphState):
+    market_researcher_prompt_template="""
+        You are an expert at Investigate the latest trends.
+        Investigate the latest trends, hashtags, and competitor activities on 
+        Linkedin specific to the industry of this Linkedin account. 
+        Focus on gathering data that reveals what content performs well in the current year, 
+        identifying patterns, preferences, and emerging trends. 
+
+            Current date: {CURRENT_DATE}
+
+        Description of the Linkedin account for which you are doing this research: 
+            Linkedin Account Description : {LINKEDIN_ACCOUNT_DESCRIPTION}
+
+        Find the most relevant topics, hashtags and trends to use the the posts for next week. 
+    """
+
+
+    market_researcher_prompt = PromptTemplate.from_template(
+        template=market_researcher_prompt_template
+    )
+
+
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    message=state["messages"][-1].content
+
+
+    market_researcher_chain = (
+        {"LINKEDIN_ACCOUNT_DESCRIPTION": RunnablePassthrough(), "CURRENT_DATE":RunnablePassthrough()} |
+        market_researcher_prompt |
+        llm |
+        JsonOutputParser()
+    )
+
+    market_researcher_chain_input = market_researcher_chain.invoke(
+        {"LINKEDIN_ACCOUNT_DESCRIPTION" : message, "CURRENT_DATE": current_date}
+    )
+
+
 
 
 
